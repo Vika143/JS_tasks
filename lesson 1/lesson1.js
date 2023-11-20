f8c8787854f731f4ea961e4320f05efa45d0b300
Vika Borys <borysvika200@gmail.com>
2023-11-20 23:34:47 +0200

    Lesson 1

---
+++ lesson 1/lesson1.js
@@ -0,0 +1,49 @@
+let greeting = "hello";
+console.log(greeting);
+let name = "owu";
+console.log(name);
+let link= "com";
+console.log(link);
+let link1= "ua";
+console.log(link1);
+let a= 1;
+console.log(a);
+let b= 10;
+console.log(b);
+let c= -999;
+console.log(c);
+let d= 123;
+console.log(d);
+let e= 3.14;
+console.log(e);
+let k= 2.7;
+console.log(k);
+let l= 16;
+console.log(l);
+let s= true;
+console.log(s);
+let m= false;
+console.log(m);
+
+
+
+let lastName="Borys";
+let firstName= "Victoriia";
+let middleName= "Andriivna";
+let person= lastName + " " + firstName + " " + middleName;
+console.log(person);
+
+let h= 100;
+console.log(typeof h);
+let n= '100';
+console.log(typeof n);
+let g = true;
+console.log(typeof g);
+
+let fname= prompt("What is your name????", "Name");
+console.log(fname);
+let lname= prompt("What is your last name???", "Last name");
+console.log(lname);
+let age= prompt("How old are you?", "0");
+console.log(age);
+
